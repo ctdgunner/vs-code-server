6e3e6bebcefb517a39f4e6248c0def3f0bcb9abd
ctdgunner <ctdgunner7@gmail.com>
2025-06-13 10:55:41 -0500

    move to default vscode-server directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -19,14 +19,8 @@ if [ ! -z "$GIT_USER_NAME" ] || [ ! -z "$GIT_USER_EMAIL" ]; then
     echo "Git configuration complete."
 fi
 
-# Configure SSH environment to use same VS Code directories
-echo 'export VSCODE_IPC_HOOK_CLI=/root/.vscode/cli-data' >> /root/.bashrc
-echo 'export VSCODE_SERVER_DATA_DIR=/root/.vscode/server-data' >> /root/.bashrc
-echo 'export VSCODE_USER_DATA_DIR=/root/.vscode/user-data' >> /root/.bashrc
-echo 'export VSCODE_EXTENSIONS_DIR=/root/.vscode/extensions' >> /root/.bashrc
-
 # Start SSH daemon in background
 /usr/sbin/sshd -D &
 
 # Start VS Code server in foreground
-exec code serve-web --without-connection-token --accept-server-license-terms --host 0.0.0.0 --port 8000 --cli-data-dir /root/.vscode/cli-data --user-data-dir /root/.vscode/user-data --server-data-dir /root/.vscode/server-data --extensions-dir /root/.vscode/extensions
+exec code serve-web --without-connection-token --accept-server-license-terms --host 0.0.0.0 --port 8000 --cli-data-dir /root/.vscode-server/cli-data --user-data-dir /root/.vscode-server/user-data --server-data-dir /root/.vscode-server/server-data --extensions-dir /root/.vscode-server/extensions
